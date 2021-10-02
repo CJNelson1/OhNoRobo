8831e84603180fe25781ab946626ad36e12b6be8
CJ Nelson <cjnelson1@gmail.com>
2021-10-02 18:35:17 -0500

    Fixing class names

--- Assets/Scripts/FightView.cs
+++ Assets/Scripts/FightView.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ExampleScript : MonoBehaviour
+public class FightView : MonoBehaviour
 {
     public float timeRemaining = 120;
     public bool timerIsRunning = false;
+    public Text timeText;
 
     // Start is called before the first frame update
     void Start()
@@ -22,24 +24,30 @@ public class ExampleScript : MonoBehaviour
             // It's the final count down...
             if (timeRemaining > 0)
             {
+                Debug.Log("One second has passed tho.");
                 timeRemaining -= Time.deltaTime;
             }
             else 
             {
-                Debug.Log("Time to do a fight action.");
                 timeRemaining = 0;
                 timerIsRunning = false;
+                DoAHit();
             }
         }
     }
 
-    static float getMinutes() 
+    void DisplayTime(float timeToDisplay)
     {
-        return Mathf.FloorToInt(timeRemaining / 60);
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);  
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
-    static float getSeconds()
+    void DoAHit()
     {
-        return Mathf.FloorToInt(timeRemaining % 60);
+        Debug.Log("Time to do a fight action.");
+        timeRemaining = 120;
+        timerIsRunning = true;
     }
 }
